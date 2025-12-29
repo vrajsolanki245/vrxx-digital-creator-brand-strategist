4005bb81cfffac5432d8fc144bb55edc66caad87
vrajsolanki245 <vrajsolanki245@gmail.com>
2025-12-29 17:33:09 +0530

    production done

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,14 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
       },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      base: './'
     };
 });
